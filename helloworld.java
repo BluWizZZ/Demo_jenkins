e01777911b0bc9fd572729ecc63c481fb3ce54de
BluWizZZ <142484712+BluWizZZ@users.noreply.github.com>
2024-02-23 12:09:58 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,9 @@
-// This is a simple Java program.
+;// This is a simple Java program.
 // FileName : "HelloWorld.java".
  
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Hi , Hello!!!!")
+    System.out.println("Hi , Hello!!!!");
      System.out.println("How are you??");
     }
 }
